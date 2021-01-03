d5834a29ec222cd969aeb4dd16414a1ae23890a9
Prashant Maroli <prashant.maroli@evolvussolutions.com>
2021-01-03 15:55:05 +0400

    test

--- createinvoice.php
+++ createinvoice.php
@@ -4,7 +4,6 @@ require_once('head.php');
 
 require_once('bodystart.php');
 ?>
-
 <script type="text/javascript">
   var projSelected=false;
   var gProjID;
@@ -17,7 +16,6 @@ require_once('bodystart.php');
       wildcard: '%QUERY'
     }
   });
-
   $(document).ready(function() {
     $("#hidestart").hide();//show only when project is selected
     fillDropDown("api/getInvoiceFormatList.php", "#format"); //get all the invoice formats and fill dropdown
@@ -42,30 +40,24 @@ require_once('bodystart.php');
       gProjID = datum.projid;
       populateForm(datum.projid);
     }); //end of typeahead
-
     $( "#format" ) .change(function () {
       if(projSelected){
         getInvoiceInformation(gProjID);
       }
       updatePDF();
     });
-
     $('input').focusout(function(){
       updatePDF();
     });
-
     $('textarea').focusout(function(){
       updatePDF();
     });
-
     //var oTable = $('#invoiceList').dataTable();
     $('#invoiceList').dataTable().on( 'select', function ( e, dt, type, indexes ) {
       if ( type === 'row' ) {
         updatePDF();
       }
     });
-
-
     $('#geninvoice').on('click', function(event) {
       var json = buildInvoiceJSON();
       var fls  = json.invoiceproj == "" ||
@@ -83,7 +75,6 @@ require_once('bodystart.php');
         });
         return false;
       }
-
       $.ajax({
           type: "POST",
           url: "generateInvoicePDF.php",
@@ -102,22 +93,12 @@ require_once('bodystart.php');
             populateForm(gProjID);
           }
       });
-
-
     });
-
-
-
-
   });//End of ready
-
-
   function buildInvoiceJSON(){
-
     var arrInv = [];
     if ($.fn.dataTable.isDataTable('#invoiceList')) {
       var oTable = $('#invoiceList').dataTable().api();
-
       oTable.rows({selected: true}).data().toArray().forEach(function (item, index) {
         inv = {
             id:item.INVOICE_ID,
@@ -126,7 +107,6 @@ require_once('bodystart.php');
           };
         arrInv.push(inv);
       });
-
     }
     return {
       projID: gProjID,
@@ -136,11 +116,8 @@ require_once('bodystart.php');
       invDate: $('#invoicedate').val(),
       invJob: $('#invoicejob').val(),
       invCur:$('#invoicecur').val(),
-
-
       projDet:$('#invoiceproj').val(),
       projSSN:$('#invoicessn').val(),
-
       venName:$('#vendorname').val(),
       venAdd1:$('#vendoradd1').val(),
       venAdd2:$('#vendoradd2').val(),
@@ -148,14 +125,12 @@ require_once('bodystart.php');
       venTRN:$('#vendortrn').val(),
       venGST:$('#vendorgst').val(),
       venPAN:$('#vendorpan').val(),
-
       bankName:$('#bankname').val(),
       branchName:$('#branchname').val(),
       ifsc:$('#ifsc').val(),
       iban:$('#iban').val(),
       swift:$('#swift').val(),
       acctNO:$('#acctno').val(),
-
       custName:$('#customername').val(),
       custAdd1:$('#customeradd1').val(),
       custAdd2:$('#customeradd2').val(),
@@ -165,20 +140,16 @@ require_once('bodystart.php');
       custShipAdd3:$('#customershipadd3').val(),
       custTRN:$('#customertrn').val(),
       custGST:$('#customergst').val(),
-
       tax1:$('#tax1').val(),
       tax2:$('#tax2').val(),
       tax3:$('#tax3').val(),
       amtFormat:$('#amtformat').val()
-
     }
   }
-
   function updatePDF(){
     if(!projSelected){
       return false;
     }
-
     $.ajax({
         type: "GET",
         url: "invoice.php",
@@ -189,12 +160,8 @@ require_once('bodystart.php');
           $('#pdf-preview').attr('src',data.responseText);
         }
     });
-
-
   }
-
   function getInvoiceInformation(projid){
-
     $.ajax({
       dataType: "json",
       async:false,
@@ -215,16 +182,13 @@ require_once('bodystart.php');
         $('#vendortrn').val(data[0]['vendor_trn']);
         $('#vendorgst').val(data[0]['vendor_gst']);
         $('#vendorpan').val(data[0]['pan_no']);
-
         $('#customershipadd1').val(data[0]['ship_add1']);
         $('#customershipadd2').val(data[0]['ship_add2']);
         $('#customershipadd3').val(data[0]['ship_add3']);
         $('#invoicecur').val(data[0]['ccy_code']);
-
         $('#tax1').val(data[0]['tax1']);
         $('#tax2').val(data[0]['tax2']);
         $('#tax3').val(data[0]['tax3']);
-
         $('#bankname').val(data[0]['inv_bank']);
         $('#branchname').val(data[0]['inv_branch']);
         $('#ifsc').val(data[0]['inv_ifsc']);
@@ -233,17 +197,14 @@ require_once('bodystart.php');
         $('#acctno').val(data[0]['inv_acct_no']);
         $('#amtformat').val(data[0]['amt_format']);
         $('#invnoformat').val(data[0]['inv_no_format']);
-
         }
       });
   }
-
   function populateForm(projid) {
     $("#hidestart").show();
     if ($.fn.dataTable.isDataTable('#invoiceList')) {
       $('#invoiceList').DataTable().destroy();
     }
-
     table = $('#invoiceList').DataTable({
       "paging": false,
       "ordering": false,
@@ -288,7 +249,6 @@ require_once('bodystart.php');
               return data;
             }
           }
-
       ]
     });
     //End of table
@@ -302,19 +262,12 @@ require_once('bodystart.php');
         updatePDF();
       }
     });
-
     getInvoiceInformation(projid);
-
     updatePDF();
-
   }
-
 </script>
-
 <div class="container ">
-
   <div class="row row-no-gutters" >
-
   <div class="col-xs-8">
     <div class="row row-no-gutters" >
       <div class="input-group table-responsive col-xs-6 right20" id="projSearch">
@@ -327,7 +280,6 @@ require_once('bodystart.php');
         </button>
       </div>
     </div>
-
     <div id="hidestart">
     <div class="row row-no-gutters top15" >
       <div class="table-responsive col-lg-3 right10">
@@ -368,53 +320,35 @@ require_once('bodystart.php');
         </table>
       </div>
     </div>
-
-<input type="hidden" id="invoicecur" value="DEF"/>
-<input type="hidden" id="invoiceno" value="NOT GENERATED"/>
-
-
-<input type="hidden" id="vendorname" value="DEFAULT vendorname"/>
-<input type="hidden" id="vendoradd1" value="DEFAULT ADD1"/>
-<input type="hidden" id="vendoradd2" value="DEFAULT ADD2"/>
-<input type="hidden" id="vendoradd3" value="DEFAULT ADD3"/>
-<input type="hidden" id="vendortrn" value="DEFAULT TRN1"/>
-<input type="hidden" id="vendorgst" value="DEFAULT VEND GST"/>
-<input type="hidden" id="vendorpan" value="DEFAULT VEND PAN"/>
-
-
-
-<input type="hidden" id="customername" value="DEFAULT CUST NAME"/>
-<input type="hidden" id="customeradd1" value="DEFAULT ADD1"/>
-<input type="hidden" id="customeradd2" value="DEFAULT Add2"/>
-<input type="hidden" id="customeradd3" value="DEFAULT ADD3"/>
-
-<input type="hidden" id="customertrn" value="DEFAULT TRN CUST"/>
-<input type="hidden" id="customergst" value="DEFAULT GST"/>
-
-
-
-<input type="hidden" id="customershipadd1" value="DEFAULT ADD1"/>
-<input type="hidden" id="customershipadd2" value="DEFAULT ADD1"/>
-<input type="hidden" id="customershipadd3" value="DEFAULT ADD1"/>
-
-<input type="hidden" id="tax1" value="0"/>
-<input type="hidden" id="tax2" value="0"/>
-<input type="hidden" id="tax3" value="0"/>
-
-
-<input type="hidden" id="bankname" value="DEFAULT BANK NAME"/>
-<input type="hidden" id="branchname" value="DEFAULT BRANCH"/>
-<input type="hidden" id="ifsc" value="DEFAULT IFSC"/>
-<input type="hidden" id="iban" value="DEFAULT IBAN"/>
-<input type="hidden" id="swift" value="DEFAULT SWIFT"/>
-<input type="hidden" id="acctno" value="DEFAULT ACCTNO"/>
-
-<input type="hidden" id="amtformat" value="en_US"/>
-<input type="hidden" id="invnoformat" />
-
-
-
-
+    <input type="hidden" id="invoicecur" value="DEF"/>
+    <input type="hidden" id="invoiceno" value="NOT GENERATED"/>
+    <input type="hidden" id="vendorname" value="DEFAULT vendorname"/>
+    <input type="hidden" id="vendoradd1" value="DEFAULT ADD1"/>
+    <input type="hidden" id="vendoradd2" value="DEFAULT ADD2"/>
+    <input type="hidden" id="vendoradd3" value="DEFAULT ADD3"/>
+    <input type="hidden" id="vendortrn" value="DEFAULT TRN1"/>
+    <input type="hidden" id="vendorgst" value="DEFAULT VEND GST"/>
+    <input type="hidden" id="vendorpan" value="DEFAULT VEND PAN"/>
+    <input type="hidden" id="customername" value="DEFAULT CUST NAME"/>
+    <input type="hidden" id="customeradd1" value="DEFAULT ADD1"/>
+    <input type="hidden" id="customeradd2" value="DEFAULT Add2"/>
+    <input type="hidden" id="customeradd3" value="DEFAULT ADD3"/>
+    <input type="hidden" id="customertrn" value="DEFAULT TRN CUST"/>
+    <input type="hidden" id="customergst" value="DEFAULT GST"/>
+    <input type="hidden" id="customershipadd1" value="DEFAULT ADD1"/>
+    <input type="hidden" id="customershipadd2" value="DEFAULT ADD1"/>
+    <input type="hidden" id="customershipadd3" value="DEFAULT ADD1"/>
+    <input type="hidden" id="tax1" value="0"/>
+    <input type="hidden" id="tax2" value="0"/>
+    <input type="hidden" id="tax3" value="0"/>
+    <input type="hidden" id="bankname" value="DEFAULT BANK NAME"/>
+    <input type="hidden" id="branchname" value="DEFAULT BRANCH"/>
+    <input type="hidden" id="ifsc" value="DEFAULT IFSC"/>
+    <input type="hidden" id="iban" value="DEFAULT IBAN"/>
+    <input type="hidden" id="swift" value="DEFAULT SWIFT"/>
+    <input type="hidden" id="acctno" value="DEFAULT ACCTNO"/>
+    <input type="hidden" id="amtformat" value="en_US"/>
+    <input type="hidden" id="invnoformat" />
 </div>
 </div>
   <div class="col-xs-4" >
@@ -422,9 +356,6 @@ require_once('bodystart.php');
   </div>
 </div>
 </div>
-
-
-
 <?php
 
 require_once('bodyend.php');
